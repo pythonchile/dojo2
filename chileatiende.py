116130e5d738673989799fbe95ce4ffafc177522
Felipe Álvarez <falvarez@ciudadanointeligente.cl>
2012-08-09 21:44:29 -0400

    Adds custom connection and Servicio

--- chileatiende.py
+++ chileatiende.py
@@ -2,5 +2,12 @@ from urllib2 import httplib
 
 
 class ChileAtiendeClient:
-	def __init__(self, token):
-		self.connection = httplib.HTTPSConnection('www.chileatiende.cl:443')
+	def __init__(self, token, connection = None):
+
+		self.connection = connection or httplib.HTTPSConnection('www.chileatiende.cl:443')
+	def get_servicios(self):
+		pass
+
+class Servicio:
+	pass
+	
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import unittest
 from urllib2 import httplib
 
-from chileatiende import ChileAtiendeClient
-
+from chileatiende import ChileAtiendeClient, Servicio
+from mock import Mock 
 
 class ClientTest(unittest.TestCase):
 
@@ -19,6 +19,28 @@ class ClientTest(unittest.TestCase):
 		self.assertIsInstance(client.connection, httplib.HTTPSConnection)
 		self.assertEqual('www.chileatiende.cl', client.connection.host)
 		self.assertEqual(443, client.connection.port)
+
+	def test_custom_connection(self):
+		connection = Mock()
+		
+		client = ChileAtiendeClient(token = self.token, connection = connection)
+
+		self.assertEqual(connection, client.connection)
+
+	@unittest.skip("No hay tiempo")
+	def test_get_servicio_object(self):
+		client = ChileAtiendeClient(token = self.token)
+		
+		services = client.get_servicios()
+		
+		self.assertEqual([Servicio()], services)	
+
+class ServicioTest(unittest.TestCase):
+
+	def test_init(self):
+		servicio = Servicio()
+
+		self.assertIsInstance(servicio,Servicio)
 
 if __name__ == '__main__':
     unittest.main()
